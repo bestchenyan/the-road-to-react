a9aa31dcee179a0c11e2f5b6d063c863de82189b
chenyan <chenyan@zhitongits.com.cn>
2024-01-06 15:16:48 +0800

    [M]Handler Function in JSX

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -34,12 +34,18 @@ const App = () => (
   </div>
 );
 
-const Search = () => (
-  <>
-    <label htmlFor="search">Search: </label>
-    <input type="text" id="search" />
-  </>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" onChange={handleChange} />
+    </>
+  );
+};
 
 const List = () => (
   <ul>
