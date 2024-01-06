13a9df30f00380776e19047536704d420e5e4fe3
chenyan <chenyan@zhitongits.com.cn>
2024-01-06 15:42:21 +0800

    [A]React Props

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -7,32 +7,35 @@ const welcome = {
   title: "React",
 };
 
-const list = [
-  {
-    title: "React",
-    url: "https://react.js.org/",
-    auther: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectId: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    auther: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectId: 1,
-  },
-];
-const App = () => (
-  <div>
-    <h1>{welcome.greeting + welcome.title}</h1>
-    <Search />
-    <hr />
-    <List />
-  </div>
-);
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://react.js.org/",
+      auther: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectId: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      auther: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectId: 1,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>{welcome.greeting + welcome.title}</h1>
+      <Search />
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+};
 
 const Search = () => {
   const handleChange = (event) => {
@@ -47,19 +50,23 @@ const Search = () => {
   );
 };
 
-const List = () => (
+const List = ({ list }) => (
   <ul>
     {list.map((item) => (
-      <li key={item.title}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span> {item.auther} </span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
+      <Item key={item.title} item={item} />
     ))}
   </ul>
+);
+
+const Item = ({ item }) => (
+  <li key={item.title}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span> {item.auther} </span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
 );
 
 export default App;
