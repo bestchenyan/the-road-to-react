3a939b270d25261dcfc2ef03dd07b77489863738
chenyan <chenyan@zhitongits.com.cn>
2024-01-06 11:09:33 +0800

    [A]Lists in React

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -7,6 +7,25 @@ const welcome = {
   greeting: "Hey",
   title:"React"
 };
+
+const list = [
+  {
+    title: "React",
+    url: "https://react.js.org/",
+    auther: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectId:0
+  },
+    {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    auther: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points:5 ,
+    objectId:1
+  }
+];
 function App() {
 
   return (
@@ -15,6 +34,21 @@ function App() {
       
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
+
+      <ul>
+        {list.map(item => {
+          return (
+            <li key={item.title}>
+              <span> 
+                <a href={item.url}>{ item.title }</a>
+              </span>
+              <span> {item.auther} </span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
